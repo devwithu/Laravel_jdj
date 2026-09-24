523fc1ec4a0cdf0bf21fe9e93bc9f55b5bd35e16
devwithu <devwithu@naver.com>
2026-09-24 15:18:51 +0900

    ..

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\UpbitController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
 
 Route::get('/profit', [UpbitController::class, 'profit']);
